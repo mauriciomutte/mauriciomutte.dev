dba555ff67bf5cbe9574cfd88d2f8199d92ce9b4
Mauricio Mutte <litiobr@gmail.com>
2019-09-18 19:22:16 -0300

    Add gatsby-config siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,2 +1,11 @@
 module.exports = {
+  siteMetadata: {
+    title: `Maurício Mutte`,
+    author: `Maurício Mutte`,
+    description: `Maurício Mutte, 18 anos, desenvolvedor full-stack, Javascript stack, apaixonado por tecnologia e pragmático`,
+    siteUrl: ``,
+    social: {
+      twitter: `mauriciomutte`,
+    },
+  }
 }
