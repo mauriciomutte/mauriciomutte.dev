156e28710d7d79a939c81ce5709c06fa044ed600
Mauricio Mutte <litiobr@gmail.com>
2019-09-18 19:33:40 -0300

    Add plugins in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,5 +7,37 @@ module.exports = {
     social: {
       twitter: `mauriciomutte`,
     },
-  }
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          `gatsby-remark-prismjs`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ]
 }
