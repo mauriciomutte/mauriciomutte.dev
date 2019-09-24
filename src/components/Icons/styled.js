79f1448f888d781b7fc2e317a4dd9664f56c10a2
Mauricio Mutte <litiobr@gmail.com>
2019-09-23 22:56:14 -0300

    feat(icon): add icons components style

---
+++ src/components/Icons/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const NavbarIcon = styled.svg`
+  display: block;
+  width: 1.5rem;
+  height: 1.5rem;
+  margin: 0 auto 0.5rem;
+  transition: all 100ms ease-in-out;
+
+  @media (min-width: 1000px) {
+    width: 2em;
+    height: 2em;
+  }
+`;
