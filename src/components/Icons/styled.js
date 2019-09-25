90a3fba7f68947aa5b96d68704d75226fcbc33a1
Mauricio Mutte <litiobr@gmail.com>
2019-09-25 18:08:08 -0300

    feat(icon): add responsive margin

--- src/components/Icons/styled.js
+++ src/components/Icons/styled.js
@@ -4,11 +4,12 @@ export const NavbarIcon = styled.svg`
   display: block;
   width: 1.5rem;
   height: 1.5rem;
-  margin: 0 auto 0.5rem;
+  margin: 0 auto .4rem;
   transition: all 100ms ease-in-out;
 
   @media (min-width: 1000px) {
-    width: 2em;
+    margin: 0 auto .5rem;
     height: 2em;
+    width: 2em;
   }
 `;
