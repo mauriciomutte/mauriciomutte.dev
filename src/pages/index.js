ab5d630ba813b2174d91324cbd355f91af9c6a69
Mauricio Mutte <litiobr@gmail.com>
2019-09-25 18:14:02 -0300

    feat(index): add Index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,50 @@
-import React from "react"
+import React from 'react';
+import { graphql } from 'gatsby';
 
-export default () => <div>Hello world!</div>
+import Layout from '../components/Layout';
+import BlogItem from '../components/BlogItem';
+
+import { BlogSection } from '../components/BlogItem/styled';
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+      edges {
+        node {
+          excerpt
+          timeToRead
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY", difference: "")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default ({ data }) => {
+  const posts = data.allMarkdownRemark.edges;
+  console.log(posts);
+
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <BlogSection>
+        {posts.map(({ node }) => 
+          <BlogItem 
+            key={node.fields.slug}
+            link={node.fields.slug}
+            title={node.frontmatter.title}
+            date={node.frontmatter.date}
+            readTime={node.timeToRead}
+          />
+        )}
+      </BlogSection>
+    </Layout>
+  );
+}
