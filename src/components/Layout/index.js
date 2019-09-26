ad4671d6d770d5cfbf582117341064d22c0ba946
Mauricio Mutte <litiobr@gmail.com>
2019-09-26 15:37:51 -0300

    fix(layout): remove import Gatsby Link

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { useStaticQuery ,Link, graphql } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import Navbar from '../Navbar';
 import * as S from './styled';
