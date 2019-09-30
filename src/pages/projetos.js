943b03acc39fb48b58832d46a44032457393185f
Mauricio Mutte <litiobr@gmail.com>
2019-09-30 19:12:27 -0300

    feat(project): add github data fetching

--- src/pages/projetos.js
+++ src/pages/projetos.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
 
 import Layout from '../components/Layout';
@@ -12,6 +13,46 @@ const ProjectWrapper = styled.section`
 `;
 
 export default ({ location }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://api.github.com/users/mauriciomutte/repos?sort=updated&type=owner',
+      );
+
+      const removeForkRepos = () => {
+        return result.data.map((element) => {
+          if (!element.fork) {
+            return element;
+          }
+        });
+      };
+
+      const sortReposByStar = () => {
+        return removeForkRepos().sort(
+          (a, b) => (a.stargazers_count > b.stargazers_count) ? -1 : 1
+        );
+      };
+
+      const getTopSix = () => {
+        let topSix = [];
+
+        sortReposByStar().map((element, id) => {
+          if (id < 6) {
+            topSix.push(element);
+          }
+        });
+        
+        return topSix;
+      };
+
+      setData(getTopSix()); 
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <Layout path={location.pathname}>
       <h1>Projetos...</h1>
