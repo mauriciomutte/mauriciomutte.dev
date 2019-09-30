9b8182abf60cac69481e1fc3b51af36b569e43e7
Mauricio Mutte <litiobr@gmail.com>
2019-09-30 19:11:28 -0300

    feat(project): add ProjectWrapper

--- src/pages/projetos.js
+++ src/pages/projetos.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import Layout from '../components/Layout';
+
+const ProjectWrapper = styled.section`
+  margin: 3em 5%;
+
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
 
 export default ({ location }) => {
   return (
     <Layout path={location.pathname}>
       <h1>Projetos...</h1>
+
+      <ProjectWrapper>
+        
+      </ProjectWrapper>
     </Layout>
   );
 }
