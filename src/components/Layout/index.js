1de96965061b25b0064bfd8a55e9cfac3ea70928
Mauricio Mutte <litiobr@gmail.com>
2019-10-01 16:57:33 -0300

    fix(layout): remove Helmet from Layout component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,33 +1,13 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
-import { useStaticQuery, graphql } from 'gatsby';
 
 import Navbar from '../Navbar';
 import * as S from './styled';
 import GlobalStyle from '../../styles/global';
 
 export default ({ path, children }) => {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-          }
-        }
-      }
-    `
-  );
 
   return (
     <S.Layout>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <meta name="description" content={site.siteMetadata.description} />
-        <title>{site.siteMetadata.title}</title>
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet"></link>
-      </Helmet>
       <GlobalStyle />
       <Navbar path={path}/>
       <S.Main>{children}</S.Main>
