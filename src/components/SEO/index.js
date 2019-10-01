f37de106cfb9dfb0ca0bad76eab4a5d47ce2b9c3
Mauricio Mutte <litiobr@gmail.com>
2019-10-01 19:08:08 -0300

    feat(SEO): add SEO component

---
+++ src/components/SEO/index.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import {useStaticQuery, graphql} from 'gatsby'
+
+function SEO({ description, lang, meta, title, image }) {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+          }
+        }
+      }
+    `
+  );
+
+  const metaDescription = description || site.siteMetadata.description;
+  const ogImage = image || 'https://mauriciomutte.github.io/assets/og-image.jpg';
+
+  return(
+    <Helmet 
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription
+        },
+        {
+          property: `og:image`,
+          content: ogImage
+        },
+        {
+          property: `og:title`,
+          content: `${title} | ${site.siteMetadata.title}`
+        },
+        {
+          property: `og:description`,
+          content: metaDescription
+        },
+        {
+          property: `og:type`,
+          content: `website`
+        },
+        {
+          name: `twitter:card`,
+          content: `summary_large_image`
+        },
+        {
+          name: `twitter:image`,
+          content: ogImage
+        },
+        {
+          name: `twitter:title`,
+          content: `${title} | ${site.siteMetadata.title}`
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription
+        }
+      ].concat(meta)}
+    />
+  );
+}
+
+SEO.defaultProps = {
+  lang: `pt-br`,
+  meta: [],
+  description: ``,
+}
+
+export default SEO;
