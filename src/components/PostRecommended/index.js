d426f32ecc1a74b5050bc585f17ea8f673f577c5
Mauricio Mutte <litiobr@gmail.com>
2019-12-04 19:30:10 -0300

    Feat(PostRecommended): create new component

---
+++ src/components/PostRecommended/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import * as S from './styled';
+
+export default ({next, previous}) => {
+  return (
+    <S.RecommendedWrapper>
+      {previous && (
+        <S.Item>
+          <S.Title>Anterior</S.Title>
+          <S.RecommendedLink
+            to={previous.fields.slug}
+          >
+            {previous.frontmatter.title}
+          </S.RecommendedLink>
+        </S.Item>
+        
+      )}
+      {next && (
+        <S.Item>
+          <S.Title>Próximo</S.Title>
+          <S.RecommendedLink
+            to={next.fields.slug}
+          >
+            {next.frontmatter.title}
+          </S.RecommendedLink>
+        </S.Item>
+      )}
+    </S.RecommendedWrapper>
+  );
+}
