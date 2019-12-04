df4aca1823e453dd4a7dd0ae9480a7117ae22fb6
Mauricio Mutte <litiobr@gmail.com>
2019-12-04 19:28:52 -0300

    Feat(gatsby-node): add previous and next

--- gatsby-node.js
+++ gatsby-node.js
@@ -24,6 +24,22 @@ exports.createPages = async ({ graphql, actions }) => {
               slug
             }
           }
+          next {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+            }
+          }
+          previous {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+            }
+          }
         }
       }
     }
@@ -32,12 +48,15 @@ exports.createPages = async ({ graphql, actions }) => {
   // Create blog posts pages.
   const posts = result.data.allMarkdownRemark.edges;
 
-  posts.forEach(({ node }) => {
+  posts.forEach(({ node, next, previous }) => {
     createPage({
       path: node.fields.slug,
       component: path.resolve(`./src/templates/blog-post.js`),
       context: {
         slug: node.fields.slug,
+        // the order is different here because of the DESC order
+        previous: next,
+        next: previous
       },
     });
   });
