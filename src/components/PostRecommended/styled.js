34f0de0b5490a3542d105bcb370327ca3f68e50a
Mauricio Mutte <litiobr@gmail.com>
2019-12-05 23:37:11 -0300

    Feat(PostRecommended): add border and padding

--- src/components/PostRecommended/styled.js
+++ src/components/PostRecommended/styled.js
@@ -3,11 +3,18 @@ import styled from 'styled-components';
 import { Link } from 'gatsby';
 
 export const RecommendedWrapper = styled.section`
+  border-bottom: 1px solid #fff1; 
+  border-top: 1px solid #fff1;
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-template-rows: 1fr;
   grid-column-gap: 1em;
+  padding: 3em 0;
   overflow: hidden;
+
+  @media (min-width: 700px) {
+    padding: 3em 4em
+  }
 `;
 
 export const Item = styled.div`
