66619953407fe2195daf859f1591b40797c1fcbb
mauriciomutte <litiobr@gmail.com>
2020-01-23 23:29:44 -0300

    :art: Add page 404 export function

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 
-export default () => {
+export default function page404() {
   return (
     <Layout>
       <SEO title="404" />
