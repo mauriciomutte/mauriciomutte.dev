2cccc896e7b2b41eb85101c91f53d75a4ac6ba56
mauriciomutte <litiobr@gmail.com>
2020-01-23 23:27:45 -0300

    :art: Add SEO default export

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import {useStaticQuery, graphql} from 'gatsby';
 
-function SEO({ description, lang, meta, title, image }) {
+export default function SEO({ description, lang, meta, title, image }) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -87,5 +87,3 @@ SEO.defaultProps = {
   meta: [],
   description: ``,
 }
-
-export default SEO;
