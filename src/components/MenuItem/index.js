69bd9b6e9491560bb546ce631df187c85880afbd
mauriciomutte <litiobr@gmail.com>
2020-01-23 23:22:43 -0300

    :art: Add MenuItem function

--- src/components/MenuItem/index.js
+++ src/components/MenuItem/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import * as S from './styled';
 
-export default ({ children, to, name, isActive }) => {
+export default function MenuItem({ children, to, name, isActive }) {
   return (
     <S.MenuItem>
       <S.MenuLink to={to} isActive={isActive}>
