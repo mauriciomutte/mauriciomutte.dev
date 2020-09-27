23d68bf4590ace21e8618826b7828bb98064aa31
mauriciomutte <litiobr@gmail.com>
2020-09-27 20:16:43 -0300

    🚧 add about me content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,47 @@
 import React from 'react';
 
+import MeImg from '../../content/assets/me.jpeg';
+
 import SEO from '../components/SEO/SEO';
 import Layout from '../components/Layout';
+import { AboutMeWrapper, AboutMeSection } from '../styles/base';
 
 function AboutPage({ location }) {
   return (
     <Layout path={location.pathname}>
       <SEO title="Sobre" />
-      <h1>AboutPage</h1>
+      <AboutMeWrapper>
+        <h1>Sobre mim</h1>
+
+        <AboutMeSection>
+          <img src={MeImg} alt="" />
+          <div>
+            <h3>Olá, que honra ter você aqui!</h3>
+            <p>
+              Meu nome é Maurício Mutte, tenho 19 anos, nasci em Porto Alegre/RS
+              (aka Gaúcho), sou desenvolvedor Full Stack e apaixonado por
+              projetos open source.
+            </p>
+            <p>
+              Sou fascinado por construir coisas novas e uso a programação como
+              ferramenta para tal. Por isso, comecei a programar cedo (quando
+              estava entrando no Ensino Médio). Ao terminar essa fase, tive
+              oportunidades para aplicar meu conhecimento em projetos reais.
+            </p>
+            <p>
+              Sou bem comunicativo, adoro trabalhar em equipe e estou sempre
+              compartilhando coisas que aprendo e julgo interessante. Por isso,
+              criei esse blog: para ajudar pessoas e como forma de aplicar o
+              "Learn In Public".
+            </p>
+            <p>
+              Além de programar, gosto muito de contribuir em projetos open
+              source, ouvir músicas, ler livros, jogar vídeo game ( CS:GO
+              principalmente ) e assistir séries.
+            </p>
+          </div>
+        </AboutMeSection>
+      </AboutMeWrapper>
     </Layout>
   );
 }
---
+++ src/styles/base.js
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+import media from 'styled-media-query';
+
+export const AboutMeWrapper = styled.div`
+  color: var(--post-text);
+  text-align: center;
+  padding: 1rem;
+
+  h1,
+  h2 {
+    font-weight: var(--font-xbold);
+    margin-bottom: 2rem;
+  }
+
+  h1 {
+    font-size: 36px;
+  }
+
+  h1 {
+    font-size: 32px;
+  }
+`;
+
+export const AboutMeSection = styled.div`
+  align-items: center;
+  background: var(--post-bg);
+  border-radius: 10px;
+  display: flex;
+  margin: auto;
+  max-width: 900px;
+  padding: 1rem 2rem;
+
+  :hover {
+    img {
+      border: 5px solid var(--secondary);
+    }
+  }
+
+  ${media.lessThan('medium')`
+    flex-direction: column;
+  `}
+
+  h3 {
+    font-size: 26px;
+    font-weight: var(--font-xbold);
+    margin: 2rem 0 1rem;
+  }
+
+  img {
+    border: 5px solid var(--primary);
+    border-radius: 50%;
+    height: 250px;
+    transition: 3s;
+    width: auto;
+
+    ${media.greaterThan('medium')`
+      margin-right: 2rem;
+    `}
+  }
+
+  p {
+    font-size: 1.1rem;
+    line-height: 1.5;
+    margin: 10px 0 20px;
+    text-align: left;
+  }
+`;
