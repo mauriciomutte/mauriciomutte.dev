eadaaf4d9833f625cdfadb2233c2e1d6f977c8f1
mauriciomutte <litiobr@gmail.com>
2021-01-28 22:39:56 -0300

    feat: add social links

---
+++ src/components/SocialLink/content.js
@@ -0,0 +1,20 @@
+const links = [
+  {
+    label: 'Github',
+    url: 'https://github.com/mauriciomutte',
+  },
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/mauriciomutte',
+  },
+  {
+    label: 'Linkedin',
+    url: 'https://linkedin.com/in/mauriciomutte/',
+  },
+  {
+    label: 'Instagram',
+    url: 'https://instagram.com/mauriciomutte',
+  },
+];
+
+export default links;
