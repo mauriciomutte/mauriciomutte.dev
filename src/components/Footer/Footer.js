341e901e7b842c2822546cc8793699998e9e8006
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2021-11-15 22:37:35 -0300

    feat(components): add Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,28 @@
+import styles from './Footer.module.css'
+
+const links = [
+  {
+    title: 'Github',
+    url: 'https://github.com/mauriciomutte',
+  },
+  {
+    title: 'Twitter',
+    url: 'https://twitter.com/mauriciomutte',
+  },
+  {
+    title: 'LinkedIn',
+    url: 'https://linkedin.com/in/mauriciomutte/',
+  },
+]
+
+const Footer = () => (
+  <footer className={styles.footer}>
+    {links.map(({ title, url }) => (
+      <a href={url} target="_blank" className={styles.footer__link}>
+        {title}
+      </a>
+    ))}
+  </footer>
+)
+
+export default Footer
