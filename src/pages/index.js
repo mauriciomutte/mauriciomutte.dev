e9f536735b24c92f6bff9c700d0b2d228494e725
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2021-11-15 22:47:42 -0300

    feat(pages): add Home styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
+import Main from '../layouts/Main'
+import styles from '../styles/Home.module.css'
+
 function HomePage() {
-  return <div>Welcome to mauriciomutte.dev!</div>;
+  return (
+    <div className={styles.home__wrapper}>
+      <h1 className={styles.home__title}>Maurício Mutte</h1>
+      <p className={styles.home__description}>
+        Desenvolvedor Front-end na Juntos Somos Mais.
+      </p>
+    </div>
+  )
 }
 
-export default HomePage;
+HomePage.Layout = Main
+
+export default HomePage
