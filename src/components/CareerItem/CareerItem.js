b25855e5453a94a29b0aa9565a41c0d9384a12ef
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2021-11-20 16:09:05 -0300

    feat: add format period date

--- src/components/CareerItem/CareerItem.js
+++ src/components/CareerItem/CareerItem.js
@@ -1,11 +1,23 @@
+import { format, parseISO } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
 import styles from './CareerItem.module.css'
 
 const CareerItem = ({ occupation, company, startDate, endDate }) => {
+  const formatDate = (date) =>
+    format(parseISO(date), 'MMM yyyy', { locale: ptBR })
+
+  const getPeriodDate = () => {
+    if (!endDate) return `${formatDate(startDate)} - Present`
+
+    return `${formatDate(startDate)} - ${formatDate(endDate)}`
+  }
+
   return (
     <section className={styles.career__section}>
       <h2 className={styles.career__occupation}>{occupation}</h2>
       <span className={styles.career__details}>
-        {company} • {startDate} - {endDate || 'Presente'}
+        {company} • {getPeriodDate()}
       </span>
     </section>
   )
