01a0b1f81567aae42a3a2837d179d4a3cea56312
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2021-11-20 14:17:28 -0300

    feat: add career about data

---
+++ src/data/about.js
@@ -0,0 +1,15 @@
+const career = [
+  {
+    occupation: 'Front-end Engineer',
+    company: 'Juntos Somos Mais',
+    startDate: '2021-06-01',
+  },
+  {
+    occupation: 'Full-stack Engineer',
+    company: 'Tim Brasil',
+    startDate: '2020-01-01',
+    endDate: '2021-05-01',
+  },
+]
+
+export default career
