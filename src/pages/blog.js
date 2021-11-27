c60fd154b361846dc43ebf0384502a49720f0bfd
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2021-11-27 20:04:02 -0300

    feat: add BlogItem posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,14 @@
 import Head from 'next/head'
 
+import { getAllPosts } from '../lib/blog'
+import BlogItem from '../components/BlogItem/BlogItem'
 import Main from '../layouts/Main'
 
 function Blog(props) {
-  const { title, description } = props
+  const { title, description, posts } = props
 
   return (
-    <div>
+    <div style={{ width: '100%' }}>
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
@@ -15,16 +17,31 @@ function Blog(props) {
         <meta property="og:url" content="https://mauriciomutte.dev/sobre/" />
       </Head>
 
-      <h1>Soon</h1>
+      <h1>Todas as publicações</h1>
+      <div style={{ margin: '0 -24px' }}>
+        {posts.map((post) => (
+          <BlogItem
+            key={post.slug}
+            slug={post.slug}
+            date={post.formattedDate}
+            tags={post.tags}
+            title={post.title}
+            description={post.description}
+          />
+        ))}
+      </div>
     </div>
   )
 }
 
 export async function getStaticProps() {
+  const posts = getAllPosts()
+
   return {
     props: {
       title: 'Blog | Maurício Mutte',
       description: 'Publicações de Maurício Mutte',
+      posts,
     },
   }
 }
