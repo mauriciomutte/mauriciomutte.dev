fc3975d6af9bcce3774d012c693c0f4faf7e106b
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-03-12 19:03:40 -0300

    post: 2021-2022

--- src/lib/blog.js
+++ src/lib/blog.js
@@ -34,7 +34,7 @@ export function getAllPosts() {
   const slugs = fs.readdirSync(postsDirectory)
   const posts = slugs
     .map((slug) => getPostBySlug(slug))
-    .sort((post1, post2) => (post1.date > post2.date ? '-1' : '1'))
+    .sort((post1, post2) => (post1.date > post2.date ? '1' : '-1'))
 
   return posts
 }
