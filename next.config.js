ede19fd8a4f0f25dfab925f252f39bea248e709e
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-08-07 18:00:09 -0300

    chore: add Next rewrites og image

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: ['github.com'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/og-image.png',
+        destination: '/api/og-image',
+        has: [{ type: 'query', key: 'title' }],
+      },
+    ]
+  },
 }
