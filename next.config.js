ad5dda319c294c427ca8765752a3776b0c5256ab
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-09-03 21:29:55 -0300

    fix: remove og image redirect in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,13 +2,4 @@ module.exports = {
   images: {
     domains: ['github.com'],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/og-image.png',
-        destination: '/api/og-image',
-        has: [{ type: 'query', key: 'title' }],
-      },
-    ]
-  },
 }
