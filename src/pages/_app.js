9c9663042a7b7aa9b797c349f91bb699c2dd66ab
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2023-01-06 13:10:45 -0300

    feat: move to `@next/font`

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
+import { Inter, Fira_Code } from '@next/font/google'
+
 import '../styles/global.css'
 import '../../public/static/css/prism.css'
+
+const fontInter = Inter({ subsets: ['latin'] })
+const fontFiraCode = Fira_Code({ subsets: ['latin'] })
 
 const Noop = ({ children }) => children
 
@@ -7,8 +12,18 @@ export default function MyApp({ Component, pageProps }) {
   const Layout = Component.Layout || Noop
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --inter-font: ${fontInter.style.fontFamily};
+            --fira-code-font: ${fontFiraCode.style.fontFamily};
+          }
+        `}
+      </style>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
