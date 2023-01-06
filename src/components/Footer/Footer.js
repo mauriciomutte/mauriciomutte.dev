95519d9930ebc86cea39bba5571e28673c79cc3a
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2023-01-06 13:38:29 -0300

    fix: add alt in Footer social icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,29 +1,31 @@
+import Image from 'next/image'
+
 import styles from './Footer.module.css'
 
 const links = [
   {
     title: 'Github',
     url: 'https://github.com/mauriciomutte',
-    icon: '/static/images/social-github.svg'
+    icon: '/static/images/social-github.svg',
   },
   {
     title: 'Twitter',
     url: 'https://twitter.com/mauriciomutte',
-    icon: '/static/images/social-twitter.svg'
+    icon: '/static/images/social-twitter.svg',
   },
   {
     title: 'LinkedIn',
     url: 'https://linkedin.com/in/mauriciomutte/',
-    icon: '/static/images/social-linkedin.svg'
+    icon: '/static/images/social-linkedin.svg',
   },
 ]
 
 const Footer = () => (
   <footer className={styles.footer}>
-    {links.map(({title, url, icon}) => (
+    {links.map(({ title, url, icon }) => (
       <a key={title} href={url} target="_blank" className={styles.footer__link}>
         <span>{title}</span>
-        <img height="16" width="16" src={icon}/>
+        <Image height="16" width="16" src={icon} alt={`${title} logo`} />
       </a>
     ))}
   </footer>
