fccc49bf5a123e7054e21703365e2fedf8a6ff67
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2023-05-06 18:09:02 -0300

    feat(app): add home page

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import styles from '@/styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.home__wrapper}>
+      <h1 className={styles.home__title}>Maurício Mutte</h1>
+      <p className={styles.home__description}>
+        Frontend Software Engineer at Juntos Somos Mais
+      </p>
+    </div>
+  )
+}
