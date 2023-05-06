d024d9a491ce3e00b24a6efd17120db3b2128282
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2023-05-06 17:24:56 -0300

    feat(app): add  `root layout` with fonts

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import { Inter } from 'next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className={inter.className}>
+      <body>{children}</body>
+    </html>
+  )
+}
