09f64ac70252229b6ee8002859dd0161ce202b3c
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2023-05-07 14:17:57 -0300

    feat: migrate lib files to TypeScript

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -1,15 +1,15 @@
 import fs from 'fs'
 import { join } from 'path'
-import { remark } from 'remark'
+// import { remark } from 'remark'
 import { format } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
-import html from 'remark-html'
-import prism from 'remark-prism'
+// import html from 'remark-html'
+// import prism from 'remark-prism'
 import matter from 'gray-matter'
 
 const postsDirectory = join(process.cwd(), 'posts')
 
-export function getPostBySlug(slug) {
+export function getPostBySlug(slug: string) {
   if (!slug) return null
 
   const realSlug = slug.replace(/\.md$/, '')
@@ -31,19 +31,18 @@ export function getPostBySlug(slug) {
 }
 
 export function getAllPosts() {
-  const slugs = fs.readdirSync(postsDirectory)
-  const posts = slugs
-    .map((slug) => getPostBySlug(slug))
-    .sort((post1, post2) => (post1.date > post2.date ? '-1' : '1'))
+  const slugs: string[] = fs.readdirSync(postsDirectory)
+
+  const posts = slugs.map((slug: string) => getPostBySlug(slug))
 
   return posts
 }
 
-export async function convertMarkdownToHtml(markdown) {
-  const result = await remark()
-    .use(html, { sanitize: false })
-    .use(prism)
-    .process(markdown)
+// export async function convertMarkdownToHtml(markdown: any) {
+//   const result = await remark()
+//     .use(html, { sanitize: false })
+//     .use(prism)
+//     .process(markdown)
 
-  return result.toString()
-}
+//   return result.toString()
+// }
