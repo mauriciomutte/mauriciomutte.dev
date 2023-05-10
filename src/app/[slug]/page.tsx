6f2cf199e74f0433aadd39025e7360c23a9989e1
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2023-05-09 22:25:13 -0300

    feat(og): add dynamic og in post page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -28,9 +28,34 @@ export async function generateMetadata({
 
   if (!post) return {}
 
+  const url = process.env.NEXT_PUBLIC_APP_URL
+
+  const ogUrl = new URL(`${url}/api/og`)
+  ogUrl.searchParams.set('title', post.title)
+
   return {
     title: post.title,
     description: post.description,
+    openGraph: {
+      title: post.title,
+      description: post.description,
+      type: 'article',
+      url: `${url}${post.slug}`,
+      images: [
+        {
+          url: ogUrl.toString(),
+          width: 1200,
+          height: 630,
+          alt: post.title,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: post.title,
+      description: post.description,
+      images: [ogUrl.toString()],
+    },
   }
 }
 
