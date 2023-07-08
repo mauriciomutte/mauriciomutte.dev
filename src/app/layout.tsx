707c8561def422789fbe8df1a7e06b7a127f24fd
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2023-07-08 16:56:52 -0300

    fix: add ga page_path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,9 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}');
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
           `}
         </NextScript>
       </Head>
