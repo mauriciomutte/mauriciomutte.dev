84d9931a293a24cefb0446b1f072f517492848a8
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2023-09-08 13:03:15 -0300

    fix: contentlayer plugins breakchanges

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -53,17 +53,11 @@ export default makeSource({
         rehypePrettyCode,
         {
           theme: 'github-dark',
-          onVisitLine(node) {
-            // Prevent lines from collapsing in `display: grid` mode, and allow empty
-            // lines to be copy/pasted
-            if (node.children.length === 0) {
-              node.children = [{ type: 'text', value: ' ' }]
-            }
-          },
+          keepBackground: false,
           onVisitHighlightedLine(node) {
             node.properties.className.push('line--highlighted')
           },
-          onVisitHighlightedWord(node) {
+          onVisitHighlightedChars(node) {
             node.properties.className = ['word--highlighted']
           },
         },
