2accdcce6a44528bf7b3f54b7f37650200dc2a13
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2023-09-08 13:07:50 -0300

    fix(tailwind-config): add specifc content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './src/app/**/*.{ts,tsx}',
+    './src/ui/**/*.{ts,tsx}',
+    './posts/**/*.{md,mdx}',
+  ],
   theme: {
     extend: {},
   },
