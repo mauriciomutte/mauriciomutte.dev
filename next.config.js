efd5486550daec878ee5e42d678eb5a0c8f2f7f3
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2023-10-11 22:37:56 -0300

    fix(next): remove experimental `appDir` option from next config
    As it is not required since version 13.4

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const { withContentlayer } = require('next-contentlayer')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
     typedRoutes: true,
   },
   images: {
